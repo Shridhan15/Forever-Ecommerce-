e694c314e6fa4e163c3cd20d62707b670ef87ae0
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-05-02 19:39:57 +0530

    cart total

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -60,6 +60,25 @@ const ShopContextProvider = (props) => {
     setCartItems(cartData)
   }
 
+  const getCartAmount =  () =>{
+
+    let totalAmount= 0;
+    for(const items in cartItems){
+      let itemInfo= products.find((product)=>product._id=== items)
+      for( const item in cartItems[items]){
+        try {
+            if(cartItems[items][item]>0){
+              totalAmount+= itemInfo.price * cartItems[items][item]
+            }
+        } catch (error) {
+          
+        }
+      }
+    }
+
+    return totalAmount;
+  }
+
   useEffect(()=>{
     console.log(cartItems)
   },[cartItems])
@@ -75,6 +94,8 @@ const ShopContextProvider = (props) => {
     addToCart,
     getCartCount,
     updateQuantity,
+    getCartAmount,
+    
   };
 
   return (
